1552533f34ec07ad5e9bcf4cbbe29a6954f92b5f
Patrick Renschler <patrick.renschler@gmail.com>
2014-03-18 14:43:08 +0530

    update

--- R/webtool.R
+++ R/webtool.R
@@ -1,11 +1,11 @@
+#' @title
 #' Start the pqantimalarials interactive web tool.
-
 #' @description
 #' This function starts the interactive web tool which
 #' runs locally on the user's machine. If the browser doesn't
 #' open automatically, the function provides the url so you can
 #' manually open your browser and view the site.
-
+#' @seealso \code{\link{runApp}} which this function wraps.
 #' @details
 #' Within the web tool, the left panel allows users to set input
 #' parameters that are then used to estimate the number of under-five
@@ -15,13 +15,16 @@
 #' explore the outputs from uncertainty and sensitivity analyses that
 #' were performed using their input settings. Users can download the
 #' output data and visualizations as CSVs and PDFs respectively.
-
+#' @examples
+#' \dontrun{
+#' # Start the web tool
+#' webtool()
+#' }
 #' @references
 #' J. Patrick Renschler, Kelsey Walters, Paul Newton,
 #'   Ramanan Laxminarayan. "Estimated under-five deaths
 #'   associated with poor-quality antimalarials in
 #'   sub-Sarahan Africa". 2014.
-
 #' @export
 
 webtool <- function(){
